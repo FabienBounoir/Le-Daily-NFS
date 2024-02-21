c5051ce978e9ff9f665d71f451ad257fe6bb1f0c
FabienBounoir <bounoirfabien@gmail.com>
2024-02-21 01:38:14 +0100

    fix: 🐛 update timer in BDD

--- src/routes/_api/sessions/me/+server.js
+++ src/routes/_api/sessions/me/+server.js
@@ -24,8 +24,8 @@ export const PUT = async ({ request }) => {
 		throw error(401, "Unauthorized");
 	}
 
-	const { color, time, speakers } = await request.json();
-	const newUser = await userService.update(user._id, { color, time, speakers });
+	const { color, timer, speakers } = await request.json();
+	const newUser = await userService.update(user._id, { color, timer, speakers });
 
 	return json(newUser);
 };
